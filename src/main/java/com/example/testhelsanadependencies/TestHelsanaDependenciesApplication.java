da799503d3a1d333cba255e4bda2f810fad287c4
Zoltan Altfatter <altfatterz@gmail.com>
2019-01-23 14:42:09 +0100

    test config

---
+++ src/main/java/com/example/testhelsanadependencies/TestHelsanaDependenciesApplication.java
@@ -0,0 +1,14 @@
+package com.example.testhelsanadependencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHelsanaDependenciesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHelsanaDependenciesApplication.class, args);
+    }
+
+}
+
